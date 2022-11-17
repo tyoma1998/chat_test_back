007ca76dd162112863fb55861fd6b701f3671f4f
Artemiy Levitskiy <alevitskiy@jaxel.com>
2022-11-17 18:06:43 +0300

    fixed cors for socket

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "https://chat-test-task-fron.herokuapp.com/",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
